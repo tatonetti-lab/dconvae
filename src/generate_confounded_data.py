1ea9f712c4b9137900c428997d415c934c5e6087
Nicholas Tatonetti <nick.tatonetti@gmail.com>
2024-10-28 15:12:55 -0700

    updated confounded data script, cleaned up data; added data to gitignore

--- src/generate_confounded_data.py
+++ src/generate_confounded_data.py
@@ -18,10 +18,6 @@ import matplotlib.pyplot as plt
 
 import sklearn.metrics
 
-# TODO: Refactor this so that the datasets are created separately from when they're saved to disk.
-# TODO: This will support if/when we want to call the generation directly instead of using the file
-# TODO: system as an intermediary. 
-
 # TODO: Add confounding by co-prescription. 
 # TODO: Add confounding by age.
 # TODO: Add confounding by sex. 
@@ -88,47 +84,7 @@ def build_dataframe(matrices, ordered_m1, m1_name, ordered_m2, m2_name, m12label
 
     return pd.DataFrame(dfdata)
 
-def parse_args():
-    parser = argparse.ArgumentParser(description='Simulate and analyze drug-reaction relationships')
-    
-    # Data generation parameters
-    parser.add_argument('--ndrugs', type=int, default=25,
-                        help='Number of unique drugs (default: 25)')
-    parser.add_argument('--nreactions', type=int, default=50,
-                        help='Number of unique reactions (default: 50)')
-    parser.add_argument('--nindications', type=int, default=30,
-                        help='Number of unique indications (default: 30)')
-    parser.add_argument('--nreports', type=int, default=10000,
-                        help='Number of reports to generate (default: 10000)')
-    parser.add_argument('--proportion-true', type=float, default=0.10,
-                        help='Proportion of true drug-reaction relationships (default: 0.10)')
-    parser.add_argument('--nexamples', type=int, default=100,
-                        help='Number of confounded datasets to create (default: 100)')
-    
-    parser.add_argument('--data-dir', type=str, default='./data',
-                        help='Directory for saving output files (default: current directory)')
-    
-    return parser.parse_args()
-
-
-
-if __name__ == "__main__":
-
-    args = parse_args()
-
-    run_name  = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(8))
-
-    output_path = os.path.join(args.data_dir, f"{run_name}")
-    print(f"Saving data to: {output_path}")
-    os.makedirs(output_path, exist_ok=True)
-
-    for arg_name, arg_value in args.__dict__.items():
-        print(f"{arg_name}: {arg_value}")
-
-    config = copy.deepcopy(args.__dict__)
-    config['output_path'] = output_path
-
-    ndrugs = args.ndrugs
+def create_dataset(ndrugs, nreactions, nindications, proportion_true, nreports, nexamples):
 
     drugs = list()
     for drug in range(ndrugs):
@@ -136,8 +92,6 @@ if __name__ == "__main__":
             drugs.append(f"D{drug}")
 
     drug_probs = pd.Series(drugs).value_counts(normalize=True)
-    
-    nreactions = args.nreactions
 
     reactions = list()
     for reaction in range(nreactions):
@@ -145,8 +99,7 @@ if __name__ == "__main__":
             reactions.append(f"Rxn{reaction}")
 
     rxn_probs = pd.Series(reactions).value_counts(normalize=True)
-    
-    nindications = args.nindications
+
     indications = list()
     for indication in range(nindications):
         for _ in range(random.randint(1, 10)):
@@ -156,9 +109,6 @@ if __name__ == "__main__":
 
     drug_rxn_factors = dict()
     drug_rxn_truth = dict()
-
-    proportion_true = args.proportion_true
-
     for drug in drug_probs.index:
         for rxn in rxn_probs.index:
             if np.random.rand() <= proportion_true:
@@ -175,17 +125,7 @@ if __name__ == "__main__":
     for drug in drug_probs.index:
         drf.append([drug_rxn_factors[(drug, rxn)] for rxn in rxn_probs.index])
 
-    plt.figure(figsize=(5,2))
-    plt.hist(drug_rxn_factors.values(), bins=100)
-    plt.title('Distribution of Drug Reaction Factors')
-    plt.ylabel('Count')
-    plt.xlabel('Factor')
-    sns.despine()
-    plt.savefig(os.path.join(output_path, 'drug_rxn_factors_hist.png'))
-    plt.close()
-
-    nreports = 10000
-
+    
     reports = list()
 
     for rid in range(nreports):
@@ -194,20 +134,25 @@ if __name__ == "__main__":
         adjusted_probs /= (adjusted_probs).sum()
         rxnid = np.random.choice(len(rxn_probs), 1, True, p=adjusted_probs)[0]
         reports.append([rid, drugid, rxnid])
+    
+    # build report x drug, report by rxn
 
-    fh = open(os.path.join(output_path, 'true_reports.csv'), 'w')
-    writer = csv.writer(fh)
-    writer.writerow(['report_id', 'drug_id', 'reaction_id'])
-    writer.writerows(reports)
-    fh.close()
+    # reports by drugs
+    drugs = np.zeros(shape=(nreports, len(drug_probs)))
+    # reports by reactions
+    rxns_clean = np.zeros(shape=(nreports, len(rxn_probs)))
 
-    ind_output_path = os.path.join(output_path, 'datasets')
-    os.makedirs(ind_output_path, exist_ok=True)
+    for rid, drugid, rxnid in reports:
+        # first add the drug and the reaction
+        drugs[rid, drugid] = 1
+        rxns_clean[rid, rxnid] = 1
 
-    for i in tqdm.tqdm(range(args.nexamples)):
-
-        config[f'dataset_{i}'] = dict()
+    
+    datasets = list()
+    for i in tqdm.tqdm(range(nexamples)):
         
+        dataset = dict()
+
         ind_rxn_factors = dict()
 
         for ind in ind_probs.index:
@@ -221,15 +166,6 @@ if __name__ == "__main__":
         irf = list()
         for ind in ind_probs.index:
             irf.append([ind_rxn_factors.get((ind, rxn), 1.0) for rxn in rxn_probs.index])
-
-        plt.figure(figsize=(5,2))
-        plt.hist(ind_rxn_factors.values(), bins=100)
-        plt.title('Distribution of Indication Reaction Factors')
-        plt.ylabel('Count')
-        plt.xlabel('Factor')
-        sns.despine()
-        plt.savefig(os.path.join(ind_output_path, f'{i}_ind_rxn_factors_hist.png'))
-        plt.close()
 
         # establish correlations between drugs and indications
 
@@ -246,21 +182,14 @@ if __name__ == "__main__":
             for ind, prob in diprobs.items():
                 drug_indid_probs[drug][ind] = prob
         
-        # build report x drug, report by rxn, and report by indication matrices
-
-        # reports by drugs
-        drugs = np.zeros(shape=(nreports, len(drug_probs)))
-        # reports by reactions
-        rxns = np.zeros(shape=(nreports, len(rxn_probs)))
+        # build report by indication matrices
+        # confounded reactions
+        rxns_bias = np.zeros(shape=(nreports, len(rxn_probs)))
         # reports by indications
         inds = np.zeros(shape=(nreports, len(ind_probs)))
 
         for rid, drugid, rxnid in reports:
             
-            # first add the drug and the reaction
-            drugs[rid, drugid] = 1
-            rxns[rid, rxnid] = 1
-
             # check to see if the drug's indications add any reactions to this report
             indids, probs = zip(*drug_indid_probs[drug_probs.index[drugid]].items())
             indid = np.random.choice(indids, 1, True, p=probs)[0]
@@ -270,17 +199,10 @@ if __name__ == "__main__":
             adjusted_probs /= (adjusted_probs).sum()
             rxnid = np.random.choice(len(rxn_probs), 1, True, p=adjusted_probs)[0]
             inds[rid, indid] = 1
-            rxns[rid, rxnid] = 1
+            rxns_bias[rid, rxnid] = 1
         
-        # to load use sparse.load_npz(filepath).toarray()
-        sp.sparse.save_npz(os.path.join(ind_output_path, f'{i}_drugs.npz'), sp.sparse.csr_matrix(drugs))
-        sp.sparse.save_npz(os.path.join(ind_output_path, f'{i}_reactions.csv'), sp.sparse.csr_matrix(rxns))
-        sp.sparse.save_npz(os.path.join(ind_output_path, f'{i}_indications.csv'), sp.sparse.csr_matrix(inds))
-
-        # np.savetxt(os.path.join(ind_output_path, f'{i}_drugs.csv'), drugs, delimiter=',')
-        # np.savetxt(os.path.join(ind_output_path, f'{i}_reactions.csv'), rxns, delimiter=',')
-        # np.savetxt(os.path.join(ind_output_path, f'{i}_indications.csv'), inds, delimiter=',')
-
+        rxns = np.maximum(rxns_clean, rxns_bias)
+              
         # run some basic evals on the data we just generated
         matrices = compute_matrices(drugs, rxns, num_reports=nreports)
         drug_rxn = build_dataframe(matrices, drug_probs.index, 'drug', rxn_probs.index, 'reaction', minA=1)
@@ -310,12 +232,147 @@ if __name__ == "__main__":
         ind_merged.rename(columns={'PRR': 'PRR_ind_rxn'}, inplace=True)
         ind_reduced = ind_merged[ind_keep]
         
-        config[f'dataset_{i}']['IndPRR>10'] = (ind_reduced['PRR_ind_rxn'] > 10).sum()
-        #print(f"Ind PRRs greater than 10: {(ind_reduced['PRR_ind_rxn'] > 10).sum()}")
-
         # we only care about whent the relationship between the indication and the reaction is high
         ind_rxn_high = ind_reduced[ind_reduced['PRR_ind_rxn'] > 10]
-        # print(ind_rxn_high.shape)
+
+        dataset['ind_rxn_factors'] = ind_rxn_factors
+        dataset['rxns_observed'] = rxns
+        dataset['inds'] = inds
+        dataset['drug_rxn'] = drug_rxn
+        dataset['ind_rxn_high'] = ind_rxn_high
+        dataset['IndPRR>10'] = (ind_reduced['PRR_ind_rxn'] > 10).sum()
+        dataset['ind_drug_prr_spearman'] = sp.stats.spearmanr(ind_rxn_high['PRR_ind_ing'], ind_rxn_high['PRR_ing_rxn'])
+        dataset['Tc_spearman'] = sp.stats.spearmanr(drug_rxn['factor'], drug_rxn['Tc'])
+        dataset['PRR_spearman'] = sp.stats.spearmanr(drug_rxn['factor'], drug_rxn['PRR'])
+        dataset['Tc_AUROC'] = sklearn.metrics.roc_auc_score(drug_rxn['truth'], drug_rxn['Tc'])
+        dataset['Tc_AUPR'] = sklearn.metrics.average_precision_score(drug_rxn['truth'], drug_rxn['Tc'])
+        dataset['PRR_AUROC'] = sklearn.metrics.roc_auc_score(drug_rxn['truth'], drug_rxn['PRR'])
+        dataset['PRR_AUPR'] = sklearn.metrics.average_precision_score(drug_rxn['truth'], drug_rxn['PRR'])
+        datasets.append(dataset)
+        
+    
+    return {
+        'ndrugs': ndrugs,
+        'nreactions': nreactions,
+        'nindications': nindications,
+        'nreports': nreports,
+        'nexamples': nexamples,
+        'proportion_true': proportion_true,
+        'drug_rxn_factors': drug_rxn_factors,
+        'drug_rxn_truth': drug_rxn_truth,
+        'reports': reports,
+        'datasets': datasets,
+        'drugs': drugs,
+        'rxns': rxns_clean,
+    }
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Simulate and analyze drug-reaction relationships')
+    
+    # Data generation parameters
+    parser.add_argument('--ndrugs', type=int, default=25,
+                        help='Number of unique drugs (default: 25)')
+    parser.add_argument('--nreactions', type=int, default=50,
+                        help='Number of unique reactions (default: 50)')
+    parser.add_argument('--nindications', type=int, default=30,
+                        help='Number of unique indications (default: 30)')
+    parser.add_argument('--nreports', type=int, default=10000,
+                        help='Number of reports to generate (default: 10000)')
+    parser.add_argument('--proportion-true', type=float, default=0.10,
+                        help='Proportion of true drug-reaction relationships (default: 0.10)')
+    parser.add_argument('--nexamples', type=int, default=100,
+                        help='Number of confounded datasets to create (default: 100)')
+    
+    
+    parser.add_argument('--data-dir', type=str, default='./data',
+                        help='Directory for saving output files (default: current directory)')
+    
+    return parser.parse_args()
+
+if __name__ == "__main__":
+
+    args = parse_args()
+
+    run_name  = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(8))
+
+    output_path = os.path.join(args.data_dir, f"{run_name}")
+    print(f"Saving data to: {output_path}")
+    os.makedirs(output_path, exist_ok=True)
+
+    for arg_name, arg_value in args.__dict__.items():
+        print(f"{arg_name}: {arg_value}")
+
+    config = copy.deepcopy(args.__dict__)
+    config['output_path'] = output_path
+
+    ndrugs = args.ndrugs
+    nreactions = args.nreactions
+    nindications = args.nindications
+    proportion_true = args.proportion_true
+    nreports = args.nreports
+    nexamples = args.nexamples
+
+    # create the dataset
+    result = create_dataset(ndrugs, nreactions, nindications, proportion_true, nreports, nexamples)
+
+    drug_rxn_factors = result['drug_rxn_factors']
+
+    plt.figure(figsize=(5,2))
+    plt.hist(drug_rxn_factors.values(), bins=100)
+    plt.title('Distribution of Drug Reaction Factors')
+    plt.ylabel('Count')
+    plt.xlabel('Factor')
+    sns.despine()
+    plt.savefig(os.path.join(output_path, 'drug_rxn_factors_hist.png'))
+    plt.close()
+
+    reports = result['reports']
+
+    fh = open(os.path.join(output_path, 'true_reports.csv'), 'w')
+    writer = csv.writer(fh)
+    writer.writerow(['report_id', 'drug_id', 'reaction_id'])
+    writer.writerows(reports)
+    fh.close()
+
+    ind_output_path = os.path.join(output_path, 'datasets')
+    os.makedirs(ind_output_path, exist_ok=True)
+
+    drugs = result['drugs']
+    rxns = result['rxns']
+    # to load use sparse.load_npz(filepath).toarray()
+    sp.sparse.save_npz(os.path.join(output_path, f'drugs.npz'), sp.sparse.csr_matrix(drugs))
+    sp.sparse.save_npz(os.path.join(output_path, f'reactions.npz'), sp.sparse.csr_matrix(rxns))
+    
+    datasets = result['datasets']
+
+    for i, dataset in enumerate(datasets):
+
+        config[f'dataset_{i}'] = dict()
+        
+        ind_rxn_factors = dataset['ind_rxn_factors']
+
+        plt.figure(figsize=(5,2))
+        plt.hist(ind_rxn_factors.values(), bins=100)
+        plt.title('Distribution of Indication Reaction Factors')
+        plt.ylabel('Count')
+        plt.xlabel('Factor')
+        sns.despine()
+        plt.savefig(os.path.join(ind_output_path, f'{i}_ind_rxn_factors_hist.png'))
+        plt.close()
+
+        inds = dataset['inds']
+        rxns_observed = dataset['rxns_observed']
+        
+        sp.sparse.save_npz(os.path.join(ind_output_path, f'{i}_reactions_observed.npz'), sp.sparse.csr_matrix(rxns_observed))
+        sp.sparse.save_npz(os.path.join(ind_output_path, f'{i}_indications.npz'), sp.sparse.csr_matrix(inds))
+
+        # np.savetxt(os.path.join(ind_output_path, f'{i}_drugs.csv'), drugs, delimiter=',')
+        # np.savetxt(os.path.join(ind_output_path, f'{i}_reactions.csv'), rxns, delimiter=',')
+        # np.savetxt(os.path.join(ind_output_path, f'{i}_indications.csv'), inds, delimiter=',')
+        
+        config[f'dataset_{i}']['IndPRR>10'] = dataset['IndPRR>10']
+
+        ind_rxn_high = dataset['ind_rxn_high']
 
         plt.figure(figsize=(5,3))
         plt.scatter(ind_rxn_high['PRR_ind_ing'], ind_rxn_high['PRR_ing_rxn'], alpha=0.4)
@@ -327,7 +384,7 @@ if __name__ == "__main__":
         plt.savefig(os.path.join(ind_output_path, f'{i}_drug_ind_rxn_scatter.png'))
         plt.close()
 
-        config[f'dataset_{i}']['ind_drug_prr_spearman'] = sp.stats.spearmanr(ind_rxn_high['PRR_ind_ing'], ind_rxn_high['PRR_ing_rxn'])
+        drug_rxn = dataset['drug_rxn']
 
         plt.figure(figsize=(10,3))
         plt.subplot(1,2,1)
@@ -346,18 +403,14 @@ if __name__ == "__main__":
         plt.savefig(os.path.join(ind_output_path, f'{i}_drug_rxn_factor_scatter.png'))
         plt.close()
 
-        config[f'dataset_{i}']['Tc_spearman'] = sp.stats.spearmanr(drug_rxn['factor'], drug_rxn['Tc'])
-        config[f'dataset_{i}']['PRR_spearman'] = sp.stats.spearmanr(drug_rxn['factor'], drug_rxn['PRR'])
-        config[f'dataset_{i}']['Tc_AUROC'] = sklearn.metrics.roc_auc_score(drug_rxn['truth'], drug_rxn['Tc'])
-        config[f'dataset_{i}']['Tc_AUPR'] = sklearn.metrics.average_precision_score(drug_rxn['truth'], drug_rxn['Tc'])
-        config[f'dataset_{i}']['PRR_AUROC'] = sklearn.metrics.roc_auc_score(drug_rxn['truth'], drug_rxn['PRR'])
-        config[f'dataset_{i}']['PRR_AUPR'] = sklearn.metrics.average_precision_score(drug_rxn['truth'], drug_rxn['PRR'])
+        config[f'dataset_{i}']['ind_drug_prr_spearman'] = dataset['ind_drug_prr_spearman']
+        config[f'dataset_{i}']['Tc_spearman'] = dataset['Tc_spearman']
+        config[f'dataset_{i}']['PRR_spearman'] = dataset['PRR_spearman']
+        config[f'dataset_{i}']['Tc_AUROC'] = dataset['Tc_AUROC']
+        config[f'dataset_{i}']['Tc_AUPR'] = dataset['Tc_AUPR']
+        config[f'dataset_{i}']['PRR_AUROC'] = dataset['PRR_AUROC']
+        config[f'dataset_{i}']['PRR_AUPR'] = dataset['PRR_AUPR']
         
-        # print(f"Tc: {sp.stats.spearmanr(drug_rxn['factor'], drug_rxn['Tc'])}")
-        # print(f"PRR: {sp.stats.spearmanr(drug_rxn['factor'], drug_rxn['PRR'])}")
-        # print(f"Tc AUROC: {sklearn.metrics.roc_auc_score(drug_rxn['truth'], drug_rxn['Tc']):.3f} AUPR: {sklearn.metrics.average_precision_score(drug_rxn['truth'], drug_rxn['Tc']):.3f}")
-        # print(f"PRR AUROC: {sklearn.metrics.roc_auc_score(drug_rxn['truth'], drug_rxn['PRR']):.3f} AUPR: {sklearn.metrics.average_precision_score(drug_rxn['truth'], drug_rxn['PRR']):.3f}")
-    
     numpy_safe_json_dump(config, os.path.join(output_path, 'config.json'))
 
 
